0b77845f4f8e388573caabf7506d6ae55b31bfdb
Priyanshu <pv254424@gmail.com>
2026-05-11 17:49:11 +0530

    solved

--- src/components/common/ScrollToTop.jsx
+++ src/components/common/ScrollToTop.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const location = useLocation();
 
   useEffect(() => {
     window.scrollTo({
@@ -10,7 +10,7 @@ const ScrollToTop = () => {
       left: 0,
       behavior: 'smooth'
     });
-  }, [pathname]);
+  }, [location]);
 
   return null;
 };
